c668f66dc8a32a5f0f32d11154175d45d63c6fdf
zekka <56047021+Zeekka@users.noreply.github.com>
2019-10-31 19:15:23 +0300

    Revert "Revert "Почти закончил смену пароля""
    This reverts commit 2eafeb56c9d99cb24b59a5c3ca134befd0c5fc58.

--- kursach/src/Controller/EmailComponents/ResetPasswordController.php
+++ kursach/src/Controller/EmailComponents/ResetPasswordController.php
@@ -5,13 +5,16 @@ namespace App\Controller\EmailComponents;
 
 
 use App\Entity\User;
+use App\Service\ConfirmationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use function Sodium\compare;
 
 class ResetPasswordController extends AbstractController
 {
@@ -19,14 +22,13 @@ class ResetPasswordController extends AbstractController
      * @return Response
      * @Route("/reset", name="reset")
      */
-    public function getCodeAction(Request $request): Response
+    public function getCodeAction(Request $request, ConfirmationService $confirmationService, MailerInterface $mailer): Response
     {
+
         $form = $this->createFormBuilder()
             ->setMethod("GET")
             ->add('email', EmailType::class)
             ->add('get_code', SubmitType::class, ['label' => 'Get reset code'])
-            ->add('code', TextType::class, ['required' => false])
-            ->add('reset_password', SubmitType::class, ['label' => 'Reset password'])
             ->getForm();
 
         $form->handleRequest($request);
@@ -36,18 +38,47 @@ class ResetPasswordController extends AbstractController
             $user = $this->getDoctrine()
                 ->getRepository(User::class)
                 ->findOneBy([
-                   'email' =>  $request->query->get("form")["email"]
+                   'email' =>  $request->query->get("form")["email"],
                 ]);
 
             if (!$user){
                 throw $this->createNotFoundException("Email address does not exist");
             }
-            //TODO: spot response
-            return new Response($user->getFirstName());
+
+            $hash = $confirmationService->builtSha256($user->getEmail());
+            $user->setUniqueHash($hash);
+            $confirmationService->sendMailToUser($user, $mailer, $hash, "Reset password", 'email/reset_mail.html.twig');
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
         }
 
-        return $this->render('user.html.twig', [
+        $reset_password = $this->createFormBuilder()
+            ->setMethod("GET")
+            ->add('Reset_code', TextType::class)
+            ->add('reset', SubmitType::class)
+            ->getForm();
+
+        $reset_password->handleRequest($request);
+        if ($reset_password->isSubmitted() && $reset_password->isValid()){
+
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneBy([
+                    'uniqueHash' =>  $request->query->get("form")["Reset_code"],
+                ]);
+
+            if (!$user){
+                throw $this->createNotFoundException("Wrong reset code");
+            }
+
+            //TODO: redirect to reset password page
+                return new Response("Success");
+        }
+
+        return $this->render('email/reset_password.html.twig', [
             'form' => $form->createView(),
+            'reset_form' => $reset_password->createView(),
         ]);
     }
 }
--- kursach/src/Controller/RegistrationController.php
+++ kursach/src/Controller/RegistrationController.php
@@ -46,7 +46,7 @@ class RegistrationController extends AbstractController
             $hash = $confirmationService->builtSha256($form->getData()->getEmail());
             $user->setUniqueHash($hash);
 
-            $confirmationService->sendMailToUser($user, $mailer, $hash);
+            $confirmationService->sendMailToUser($user, $mailer, $hash, "Confirmation", 'email/register.html.twig');
 
             $em->persist($user);
             $em->flush();
