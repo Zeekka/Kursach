eaa2df3c0aef08784bd27d1c01407ebd72342851
MaksimKonoplich <gladiator8899@mail.ru>
2019-11-10 22:34:47 +0300

    add folder user_profile with twigs in templates

--- kursach/src/Controller/PostController.php
+++ kursach/src/Controller/PostController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\Post1Type;
 use App\Form\PostType;
 use App\Repository\PostRepository;
 use App\Utils\Slugger;
---
+++ kursach/src/Form/Post1Type.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Post1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('summary')
+            ->add('content')
+            ->add('publishedAt')
+            ->add('image')
+            ->add('author')
+            ->add('tags')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
